f597b6f02fe70450480accfc11f82afdfbb763e6
Hannes Lohmander <hannes@lohmander.me>
2016-05-02 20:15:33 +0200

    Add vim conf

--- setup.sh
+++ setup.sh
@@ -1,16 +1,93 @@
 #!/bin/bash
 
-# Install git
-brew install git
+function install_git {
+  echo -n "=> Installing git..."
+  brew install git &> /dev/null
+  echo -e "\t\tDone."
+}
 
-# Install Oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+function install_omz {
+  echo -n "=> Installing Oh My Zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &> /dev/null
+  echo -e "\tDone."
+}
 
-rm ~/.zshrc
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
+function create_symlinks {
+  echo -n "=> Creating symlinks..."
 
-# Install languages/platforms
-brew install nodejs
-brew install golang
+  rm ~/.zshrc
+  rm -rf ~/.vim
+  rm -rf ~/.vimrc
 
-echo "Done."
+  ln -s ~/.dotfiles/.zshrc ~/.zshrc
+  ln -s ~/.dotfiles/.vimrc ~/.vimrc
+  ln -s ~/.dotfiles/.vim ~/.vim
+
+  echo -e "\t\tDone."
+}
+
+function install_nodejs {
+  echo -n "=> Installing nodejs..."
+  brew install nodejs &> /dev/null
+  echo -e "\t\tDone."
+}
+
+function install_go {
+  echo -n "=> Installing Go..."
+  brew install golang &> /dev/null
+  echo -e "\t\tDone."
+}
+
+function install_macvim {
+  echo -n "=> Installing MacVim..."
+
+  if [ ! -d "/Applications/MacVim.app" ]; then
+    {
+      rm -rf ~/Downloads/MacVim.dmg
+      wget https://github.com/macvim-dev/macvim/releases/download/snapshot-102/MacVim.dmg -P ~/Downloads/
+      hdiutil attach ~/Downloads/MacVim.dmg
+      sudo cp -r /Volumes/MacVim/MacVim.app /Applications/MacVim.app
+      sudo cp /Volumes/MacVim/mvim /usr/local/bin/mvim
+    } &> /dev/null
+    echo -e "\t\tDone."
+  else
+    echo -e "\t\tSkipped."
+  fi
+}
+
+function install_ycm {
+  echo -e "=> Installing YouCompleteMe...\n\n"
+  start_dir=$(pwd)
+
+  cd ~/.dotfiles/.vim/bundle/YouCompleteMe
+
+  git submodule update --init --recursive
+  ./install.py --gocode-completer
+
+  cd $start_dir
+ 
+  echo -e "\n=> Done installing YouCompleteMe."
+}
+
+function setup_gitignore {
+  echo -n "=> Setting up .gitignore..."
+  
+  echo -e ".DSStore\n*.swp" > ~/.gitignore
+
+  git config --global core.excludesfile '~/.gitignore'
+  
+  echo -e "\tDone."
+}
+
+sudo echo ""
+
+install_git
+install_omz
+install_nodejs
+install_go
+install_macvim
+install_ycm
+setup_gitignore
+create_symlinks
+
+echo -e "\nAll done!"
