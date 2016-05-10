18b0d9204fb9c1d015ffbb34bfe87a219a288452
Hannes Lohmander <hannes.lohmander@starrepublic.com>
2016-05-10 10:24:42 +0200

    Add function to delete all merged git branches

--- fn.sh
+++ fn.sh
@@ -15,3 +15,12 @@ function clone {
 function dmenv {
     eval "$(dm env default)"
 }
+
+function del_merged {
+  git branch --merged \
+    | grep -v "\*" \
+    | grep -v master \
+    | grep -v develop \
+    | grep -v development \
+    | xargs -n 1 git branch -d
+}
